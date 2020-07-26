cfe8337c7b94b0d22208c008dca9ce05c8cecb93
armsiwadol69 <armsiwadol2543@gmail.com>
2020-07-26 22:47:20 +0700

    Update ex1.3 long.py

--- ex1.3 long.py
+++ ex1.3 long.py
@@ -1,3 +1,3 @@
-'''Integers
-PEP 237: Essentially, long renamed to int. That is, there is only one built-in integral type, named int;
-but it behaves mostly like the old long type.'''
+#Integers
+#PEP 237: Essentially, long renamed to int. That is, there is only one built-in integral type, named int;
+#but it behaves mostly like the old long type.
